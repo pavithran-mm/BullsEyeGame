519f244343156a6c9488ed4435b87bdddfaac79a
pavithran-mm <pavithran.mm@thoughtworks.com>
2022-09-02 15:00:41 +0530

    Add. Created UI using Storyboard

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func hitMe(){
+        let alert = UIAlertController(title: "Hello world!", message: "This is my first app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
